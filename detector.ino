8a7bbcbbfd2c2e16567910dc401e081d7c32bbea
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-04-17 10:26:56 -0400

    Tests & validation

--- detector.ino
+++ detector.ino
@@ -6,6 +6,7 @@ int concentrations[] = {0, 700, 1400, 2100, 2800, 3500, 4200, 5000}; // customiz
 String lights[] = { "r", "rg", "rb", "g", "gb", "b", "rgb" };
 
 volatile int reading;
+char* charArray;
 
 void lightLED(char led[] = "") {
     int len = sizeof(led)/sizeof(led[0]);
@@ -15,31 +16,38 @@ void lightLED(char led[] = "") {
         digitalWrite(b, LOW);
         return;
     }
-    for (int i=0; i<len i++) {
+    for (int i=0; i<len; i++) {
         digitalWrite(led[i], HIGH);
     }
 }
 
-void interpretReading() { // 7 possible light combos
+void stringToCharArray(String str) {
+    int arrayLength = str.length() + 1;
+    char buffer[arrayLength];
+    str.toCharArray(buffer, arrayLength);
+    charArray = buffer;
+}
 
+void interpretReading() { // 7 possible light combos
     Serial.print("Reading:\t");
     Serial.println(reading);
 
     if (reading < concentrations[1]) {
-        lightLED(lights[0]);
-    } else if (reading => concentrations[1] && reading < concentrations[2]) {
-        lightLED(lights[1]);
-    } else if (reading => concentrations[2] && reading < concentrations[3]) {
-        lightLED(lights[2]);
-    } else if (reading => concentrations[3] && reading < concentrations[4]) {
-        lightLED(lights[3]);
-    } else if (reading => concentrations[4] && reading < concentrations[5]) {
-        lightLED(lights[4]);
-    } else if (reading => concentrations[5] && reading < concentrations[6]) {
-        lightLED(lights[5]);
-    } else if (reading => concentrations[6]) {
-        lightLED(lights[6]);
+        stringToCharArray(lights[0]);
+    } else if (reading >= concentrations[1] && reading < concentrations[2]) {
+        stringToCharArray(lights[1]);
+    } else if (reading >= concentrations[2] && reading < concentrations[3]) {
+        stringToCharArray(lights[2]);
+    } else if (reading >= concentrations[3] && reading < concentrations[4]) {
+        stringToCharArray(lights[3]);
+    } else if (reading >= concentrations[4] && reading < concentrations[5]) {
+        stringToCharArray(lights[4]);
+    } else if (reading >= concentrations[5] && reading < concentrations[6]) {
+        stringToCharArray(lights[5]);
+    } else if (reading >= concentrations[6]) {
+        stringToCharArray(lights[6]);
     }
+    lightLED(charArray);
 
 }
 
